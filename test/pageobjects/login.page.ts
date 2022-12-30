4204b5a8150a32450c6c9a175fa0f0a0704dcf3e
gem-shubhamkumar <shubham.kumar@geminisolutions.com>
2022-12-30 14:25:19 +0530

    first-commit

--- test/pageobjects/login.page.ts
+++ test/pageobjects/login.page.ts
@@ -1,23 +1,23 @@
 import locator from "../locators/locator";
-import { expect as chaiExpect } from "chai";
+import * as action from "gem-wdio/generic/web-actions";
 
 class LoginPage {
 
     public async launchApplication () {
-        await browser.url("");
-        await browser.maximizeWindow();
-        await browser.setTimeout({ 'implicit': 20000 });
+        await action.open();
+        await action.maximizeWindow();
+        await action.setImplicitTimeout(10);
     }
 
     public async login (username: string, password: string) {
-        await (await locator.input_username).setValue(username);
-        await (await locator.input_password).setValue(password);
-        await (await locator.button_login).click();
+        await action.typeText(locator.input_username, username, "username");
+        await action.typeText(locator.input_password, password, "password");
+        await action.clickOn(locator.button_login, "login");
     }
 
     public async verifyTitle (title: string) {
-        const actualTitle:string = await (await locator.title_products).getText();
-        chaiExpect(actualTitle).equal(title);
+        const actualTitle:string = await action.getElementText(locator.title_products);
+        action.verifyEquals(title, actualTitle, "Title");
     }
 
 }
